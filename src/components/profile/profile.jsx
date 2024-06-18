098bfa1a0084f4e55520ed0d4e4bebdd775db67e
Taisiia713 <taisia197@ukr.net>
2024-06-18 12:30:24 +0200

    update

--- src/components/profile/profile.jsx
+++ src/components/profile/profile.jsx
@@ -1,24 +1,13 @@
-const userProfile = {
-  username: "Jacques Gluke",
-  tag: "jgluke",
-  location: "Ocho Rios, Jamaica",
-  avatar: "https://cdn-icons-png.flaticon.com/512/2922/2922506.png",
-  stats: {
-    followers: 5603,
-    views: 4827,
-    likes: 1308,
-  },
-};
-
 import css from "../profile/Profile.module.css";
 import UserStats from "../../components/userStats/UserStats";
 import UserData from "../../components/userData/UserData";
+import userData from "../../userData.json";
 
 export default function Profile() {
   return (
     <div className={css.container}>
-      <UserData user={userProfile} />
-      <UserStats user={userProfile} />
+      <UserData user={userData} />
+      <UserStats user={userData} />
     </div>
   );
 }
